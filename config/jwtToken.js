9c24707c138e7450b3da0fd70d2a14da9b7e7198
Swapnil Pawar <swapnilpawar2007@gmail.com>
2024-10-08 00:56:53 +0530

    Replaced Redis with JWT token for stateless session 2024-10-08

---
+++ config/jwtToken.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+class JwtToken{
+    constructor(){
+        this.secret = process.env.JWT_SECRET_KEY;
+    }
+
+
+    createJWTToken(user){
+        const token = jwt.sign(
+            {data: user, exp: Math.floor(Date.now() / 1000) + (60 * 60)}, 
+            this.secret,
+            // { algorithm: 'RS256' }
+        );
+        return token;
+    }
+
+    veriftJWTToken(token){
+        if(!token){
+            return;
+        }
+        if(!token){
+            token = req.cookies;
+        }
+        // console.log(token);
+        var decoded = "";
+        try{
+            decoded = jwt.verify(token, this.secret);
+            // console.log(decoded);
+            // req.session.user = decoded;
+            return decoded;
+        }catch(error){
+            return false;
+            // req.redirect("/login");
+        }
+        
+        
+    }
+
+}
+
+module.exports = JwtToken;
